606a44c5064eb5aea16b31eb7233c232e681cb64
1DAM <1DAM@A11-PC18>
2026-01-19 12:04:38 +0100

    yate

---
+++ Reto4AlejandroIzan/src/Reto/Yate.java
@@ -0,0 +1,25 @@
+package Reto;
+
+public class Yate extends Barco {
+	protected int camarotes;
+
+	public Yate(String nombre, Motor motor, double capacidadCombusitble, int camarotes) {
+		super(nombre, motor, capacidadCombusitble);
+		this.camarotes = camarotes;
+	}
+
+	public int getCamarotes() {
+		return camarotes;
+	}
+
+	public void setCamarotes(int camarotes) {
+		this.camarotes = camarotes;
+	}
+
+	@Override
+	public String toString() {
+		return "Yate [camarotes=" + camarotes + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}
